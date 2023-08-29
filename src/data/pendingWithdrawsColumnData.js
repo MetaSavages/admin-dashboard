2d607241df9ea60eec2df15c4bb052bb58e13b1c
alexanderbozhinov <aleksandar.bozhinov@stamsoft.com>
2023-08-29 15:56:21 +0300

    Added approve/reject pop-up to type reasons

--- src/data/pendingWithdrawsColumnData.js
+++ src/data/pendingWithdrawsColumnData.js
@@ -1,3 +1,6 @@
+import queryString from 'query-string';
+import Actions from 'layouts/payouts/components/Actions';
+
 export const pendingWithdrawsColumnData = [
     {
       Header: 'Username',
@@ -14,5 +17,21 @@ export const pendingWithdrawsColumnData = [
     {
       Header: 'Date',
       accessor: 'date'
+    },
+    {
+      Header: 'Actions',
+      accessor: 'actions',
+      sorted: false,
+      Cell: ({ row }) => {
+        const filters = {
+          users: [row.original.player_id],
+          casinos: [row.original.casino_id]
+        };
+        const queryStringParams = queryString.stringify(filters);
+        console.log(row);
+        return (
+          <Actions row={row} queryStringParams={queryString}></Actions>
+        );
+      }
     }
   ];
--- src/layouts/payouts/pending_payouts/index.js
+++ src/layouts/payouts/pending_payouts/index.js
@@ -1,16 +1,7 @@
 import Filters from './components/Filters';
 import React from 'react';
-import dataPendingPayouts from 'assets/mockData/dataPedningPayouts';
-import DataTable from 'components/DataTablePage/components/DataTable';
-import { Card } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import DashboardLayout from 'examples/LayoutContainers/DashboardLayout';
-import DashboardNavbar from 'components/DashboardNavbar';
-import Footer from 'examples/Footer';
-import MDBox from 'components/MDBox';
-import MDTypography from 'components/MDTypography';
-import Grid from '@mui/material/Grid';
 import DataTablePage from 'components/DataTablePage';
 import { getPendingWithdraws } from 'services/withdraws'
 import { pendingWithdrawsColumnData } from 'data/pendingWithdrawsColumnData'
