cfd733359d01f0b32a5a666c6a17f936022ffeef
S-Hristov <132335296+S-Hristov@users.noreply.github.com>
2023-10-27 14:19:18 +0300

    fix two factor auth

--- src/App.js
+++ src/App.js
@@ -55,7 +55,8 @@ import {
   setEmail,
   setName,
   setRole,
-  setAbility
+  setAbility,
+  setTwoFactor
 } from 'context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // Images
@@ -78,8 +79,7 @@ export default function App() {
     ability,
     name,
     email,
-    darkMode,
-    setTwoFactor
+    darkMode
   } = controller;
   const [onMouseEnter, setOnMouseEnter] = useState(false);
   const [rtlCache, setRtlCache] = useState(null);
