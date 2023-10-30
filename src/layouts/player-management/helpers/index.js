f1d6c53740fed725e19ad51629f971cdedf52886
Samuil Borisov <samuil.bo5@gmail.com>
2023-10-30 18:39:48 +0200

    show player time_spent better

--- src/layouts/player-management/helpers/index.js
+++ src/layouts/player-management/helpers/index.js
@@ -20,11 +20,14 @@ export function formatNumber(number) {
   }
   
 export function formatDuration(milliseconds) {
+    if(milliseconds == 0){
+        return '-';
+    }
     const seconds = Math.floor(milliseconds / 1000);
     const minutes = Math.floor(seconds / 60);
     const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
-  
+
     if (days > 0) {
         return `${days} day${days > 1 ? 's' : ''}`;
     } else if (hours > 0) {
