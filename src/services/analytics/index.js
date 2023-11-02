4e504fc4449f11b58b294a54ace2f20d30f61ad4
alexanderbozhinov <aleksandar.bozhinov@stamsoft.com>
2023-11-02 17:14:01 +0200

    fixed registrations chart in player activity

--- src/services/analytics/index.js
+++ src/services/analytics/index.js
@@ -204,8 +204,8 @@ export const getNewRegistrations = async () => {
 
     const data = res.data.map((registrations) => {
       return {
-        month: registrations?.intervalStart ? new Date(registrations.intervalStart).getMonth() + 1 : '-',
-        count: registrations?.count ? registrations.count : '-'
+        month: registrations?.intervalStart ? new Date(registrations.intervalStart).getMonth() : '-',
+        count: registrations?.count ? registrations.count : 0
       };
     });
 
