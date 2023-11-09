6b2bdff90ef5eac7989db929d93449c1b6df3846
alexanderbozhinov <aleksandar.bozhinov@stamsoft.com>
2023-11-09 14:17:10 +0200

    deposits service

---
+++ src/services/deposits/index.js
@@ -0,0 +1,116 @@
+import useAxios from 'hooks/useAxios';
+
+export const getDepositData = async (startDate, endDate, orderStatus, sortBy) => {
+  const api = useAxios();
+  try {
+    const result = await api.get('/orders/deposit/filtered', {
+      params: { startDate: startDate, endDate: endDate, orderStatus: orderStatus, sortBy: sortBy }
+    });
+
+    return result;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+export const getSuccesfulDeposits = async () => {
+  const api = useAxios();
+  try {
+    const unformattedData = await api.get('/coins-paid/deposits-success');
+
+    const data = unformattedData.data.data.map((deposit) => {
+      return {
+        ...deposit,
+        username: deposit?.username ? deposit.username : '-',
+        amount: deposit?.amount ? deposit.amount : '-',
+        casino: deposit?.casino ? deposit.casino : '-',
+        date: deposit?.date ? deposit.date : '-'
+      };
+    });
+
+    return {
+      data: data,
+      meta: unformattedData.data.meta
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      data: [],
+      meta: {
+        totalItems: 0,
+        itemCount: 0,
+        itemsPerPage: 0,
+        totalPages: 0,
+        currentPage: 0
+      }
+    };
+  }
+};
+
+export const getPendingDeposits = async () => {
+  const api = useAxios();
+  try {
+    const unformattedData = await api.get('/coins-paid/deposits-pending');
+
+    const data = unformattedData.data.data.map((deposit) => {
+      return {
+        ...deposit,
+        username: deposit?.username ? deposit.username : '-',
+        amount: deposit?.amount ? deposit.amount : '-',
+        casino: deposit?.casino ? deposit.casino : '-',
+        date: deposit?.date ? deposit.date : '-'
+      };
+    });
+
+    return {
+      data: data,
+      meta: unformattedData.data.meta
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      data: [],
+      meta: {
+        totalItems: 0,
+        itemCount: 0,
+        itemsPerPage: 0,
+        totalPages: 0,
+        currentPage: 0
+      }
+    };
+  }
+};
+
+export const getFailedDeposits = async () => {
+  const api = useAxios();
+  try {
+    const unformattedData = await api.get('/coins-paid/deposits-failed');
+
+    const data = unformattedData.data.data.map((deposit) => {
+      return {
+        ...deposit,
+        username: deposit?.username ? deposit.username : '-',
+        amount: deposit?.amount ? deposit.amount : '-',
+        casino: deposit?.casino ? deposit.casino : '-',
+        date: deposit?.date ? deposit.date : '-',
+        reason: deposit?.reason ? deposit.reason : '-'
+      };
+    });
+
+    return {
+      data: data,
+      meta: unformattedData.data.meta
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      data: [],
+      meta: {
+        totalItems: 0,
+        itemCount: 0,
+        itemsPerPage: 0,
+        totalPages: 0,
+        currentPage: 0
+      }
+    };
+  }
+};
