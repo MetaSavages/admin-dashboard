d34e29769cf7e1ee2db25020f140fd793a58422f
Samuil Borisov <samuil.bo5@gmail.com>
2023-11-24 14:53:14 +0200

    fix roles, bugs + autocomplete + error and more

--- src/layouts/role-management/edit-role/index.js
+++ src/layouts/role-management/edit-role/index.js
@@ -29,6 +29,9 @@ import DashboardLayout from 'examples/LayoutContainers/DashboardLayout';
 import DashboardNavbar from 'components/DashboardNavbar';
 import Footer from 'examples/Footer';
 
+// services
+import { getAllCasinos } from 'services/filters';
+
 // NewUser page components
 import RoleInfo from 'layouts/role-management/components/RoleInfo';
 
@@ -41,12 +44,14 @@ import { useEffect, useState } from 'react';
 import { Dialog, DialogActions, DialogContent, Skeleton } from '@mui/material';
 import MDTypography from 'components/MDTypography';
 import { Can } from 'context';
+import { object } from 'prop-types';
 function EditRole() {
   const { id } = useParams();
   const { formId, formField } = form;
   const currentValidation = validations[0];
   const [initialValues, setInitialValues] = useState(null);
   const [open, setOpen] = useState(false);
+  const [casinos, setCasinos] = useState([]);
 
   const handleClose = () => {
     setOpen(false);
@@ -56,26 +61,78 @@ function EditRole() {
   };
 
   const navigate = useNavigate();
+
   useEffect(() => {
-    getRole(id).then((response) => {
-      setInitialValues({
-        [formField.roleName.name]: response.data.name,
-        [formField.rolePermissions.name]: response.data.permissions.map((permission) => {
-          return {
-            value: permission.id,
-            name: `${permission.action}: ${permission.object}`
-          };
-        }),
-        [formField.casino.name]: response.data.casino
+    getAllCasinos()
+      .then((fetchedCasinos) => {
+        const modifiedCasinos = fetchedCasinos.map((casino) => ({
+          ...casino,
+          label: casino.name,
+        }));
+        modifiedCasinos.unshift({
+          blockChainId: null,
+          id: null,
+          label: 'All',
+          name: 'All',
+          provider: null,
+          value: null,
+        });
+  
+        setCasinos(modifiedCasinos);
+  
+        getRole(id)
+        .then((response) => {
+          let selectedCasino = null;
+      
+          if (response.data.casino) {
+            selectedCasino = modifiedCasinos.find((casino) => casino.id === response.data.casino.id) || null;
+          }
+      
+          if (!selectedCasino || !modifiedCasinos.find((casino) => casino.id === selectedCasino.id)) {
+            selectedCasino = modifiedCasinos.find((casino) => casino.label === 'All');
+          }
+      
+          setInitialValues({
+            [formField.roleName.name]: response.data.name,
+            [formField.rolePermissions.name]: response.data.permissions.map((permission) => ({
+              value: permission.id,
+              name: `${permission.action}: ${permission.object}`,
+            })),
+            [formField.casino.name]: selectedCasino,
+          });
+        })
+        .catch((error) => {
+          console.error('Error fetching role:', error);
+        });
+      
+      })
+      .catch((error) => {
+        console.error('Error fetching casinos:', error);
       });
-    });
+  
     return () => {
       setInitialValues(null);
     };
-  }, []);
+  }, [id, formField.roleName.name, formField.rolePermissions.name, formField.casino.name]);
 
-  const submitForm = async (values, actions) => {
-    const response = await updateRole(id, values.roleName, values.rolePermissions, values?.casino ? values.casino : null);
+  const handleSubmit = async (values, actions) => {
+    let selectedCasinoId;
+
+    if (values.casino && typeof values.casino === 'object' && values.casino.id) {
+      selectedCasinoId = values.casino.id;
+    } else if (typeof values.casino === 'string' || typeof values.casino === 'number') {
+      selectedCasinoId = values.casino;
+    } else {
+      selectedCasinoId = null;
+    }
+
+    const response = await updateRole(
+      id,
+      values.roleName,
+      values.rolePermissions,
+      selectedCasinoId
+    );
+    
     if (response.status === 200 || response.status === 201) {
       alert('Role edited successfully');
       navigate('/role-management');
@@ -86,10 +143,7 @@ function EditRole() {
       handleClose();
     }
   };
-  const handleSubmit = (values, actions) => {
-    submitForm(values, actions);
-  };
-
+  
   return (
     <>
       <Can I='update' a='role'>
